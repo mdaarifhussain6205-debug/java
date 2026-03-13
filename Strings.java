7788f3a684f1bbbb2c488a6dde86ce2902d5863c
mdaarifhussain6205-debug <mdaarifhussain6205@gmail.com>
2026-03-13 14:18:39 +0530

    update

--- Strings.java
+++ Strings.java
@@ -16,7 +16,30 @@ public class Strings {
     //   String name = sentence.substring(0,11);
 
     //  System.out.println(name);
-    StringBuilder sb = new StringBuilder("Aarif");
+    // StringBuilder sb = new StringBuilder("Aarif");
+    // System.out.println(sb);
+    // System.out.println(sb.charAt(0));
+    // sb.setCharAt(0, 't');
+    // System.out.println(sb);
+    // sb.insert(2 ,'a');
+    // System.out.println(sb);
+    // sb.delete(3,4);
+    // System.out.println(sb);
+
+    StringBuilder sb = new StringBuilder("hello");
+    for ( int i =0; i < sb.length()/2;i++){
+      int front = i;
+      int back = sb.length()-1-i;
+
+      char frontChar = sb.charAt(front);
+      char backChar = sb.charAt(back);
+
+
+      sb.setCharAt(front, backChar);
+      sb.setCharAt(back, frontChar);
+    }
     System.out.println(sb);
+
+
     }
 }
