6f023939ef95762517f8ee52b077edc9904b471d
mdaarifhussain6205-debug <mdaarifhussain6205@gmail.com>
2026-03-29 20:13:44 +0530

    add recursion file

--- Sorting.java
+++ Sorting.java
@@ -1,20 +1,28 @@
 import java.util.*;
+
 public class Sorting {
 
-
-    public static void main (String args[]){
-        int arr[] = {3,5,2,1,8,9};
-        for (int i=0; i<arr.length-1; i++){
-            for (int j=0;j<arr.length-1-i;i++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                    System.out.print(arr);
-                }
-
-            }
+    public static void printarray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
+
     
+    public static void main(String args[]) {
+        int arr[] = {6, 5, 2, 1, 8, 9};
+
+        // Insertion sorting
+        for(int i=1;i<arr.length;i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+
+        }
+        printarray(arr);
+    }
 }
