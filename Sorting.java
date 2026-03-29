4a80003916928f736c612ed793330b15f9ee53fa
mdaarifhussain6205-debug <mdaarifhussain6205@gmail.com>
2026-03-29 11:19:32 +0530

    add sorting file

--- Sorting.java
+++ Sorting.java
@@ -1,6 +1,20 @@
 import java.util.*;
 public class Sorting {
-    public static void main(String[] args) {
-        
+
+
+    public static void main (String args[]){
+        int arr[] = {3,5,2,1,8,9};
+        for (int i=0; i<arr.length-1; i++){
+            for (int j=0;j<arr.length-1-i;i++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    System.out.print(arr);
+                }
+
+            }
+        }
     }
+    
 }
