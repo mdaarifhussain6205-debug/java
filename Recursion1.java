f75c42c2b8df18cd8284825bd2234bfcbfa86a25
mdaarifhussain6205-debug <mdaarifhussain6205@gmail.com>
2026-03-31 17:39:53 +0530

    add recursion file

--- Recursion1.java
+++ Recursion1.java
@@ -1,16 +1,17 @@
-public class Recursion1 {
-    public static void calculateSum(int i, int n, int sum){
+
+public class Recursion1{
+    public static void calculateSum(int i, int n, int sum ){
         if(i==n){
-            sum +=i;
-            System.out.print(sum);
+            sum += i;
+            calculateSum(i+1,n,sum);
+            
             return;
         }
-    sum += i;
-    calculateSum(i+1, n, sum);
-       
+       System.out.println(sum);
+
     }
     public static void main ( String args []){
-        calculateSum(1, 05, 0);
-      
-    }
+        calculateSum(0,3,0);
+
+    } 
 }
